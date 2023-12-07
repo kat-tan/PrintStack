fcbe733e9cb56a392578d57a666cec33e2842b2a
kat-tank <123583375+kat-tank@users.noreply.github.com>
2023-12-06 23:53:25 -0500

    Update StackFunctions.java

--- StackFunctions.java
+++ StackFunctions.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package kat.liststackelements;
 
 /**
@@ -10,20 +6,22 @@ package kat.liststackelements;
  */
 public class StackFunctions {
     
-    // 
+    // Creating array to store input values in the stack and top for pointing in the stack.
     private String [] stack;  
     private int top;   
    
+    // Initializes stack with the value entered by the user.
     public StackFunctions (int capacity) {
         stack = new String[capacity];
         top = 0;
     }
     
-    // If user did not enter anything, sta
+    // If user did not enter anything, stack is empty.
     public boolean empty() {
         return top == 0;
     }
     
+    // Points to top of the stack to add input.
     public void push(String input) {
         if (top == stack.length) {
            throw new StackOverFlowException();
@@ -33,7 +31,8 @@ public class StackFunctions {
           top ++;           
         }         
     }
-   
+    
+    // Finds the top of the stack to remove and return the values to then print out.
     public String pop() {
         if (empty()) {
            throw new EmptyStackException();
